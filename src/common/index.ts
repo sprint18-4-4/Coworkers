181d366720781d46f35dc4ec36d25b2b2ff846b4
Kim_Won_Seon <gws8221@gmail.com>
2025-11-08 22:34:09 +0900

    chore: 배럴패턴 추가

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Input } from "./Input/Input";
 export { default as BaseButton } from "./Button/BaseButton";
+export { default as InputBox } from "./InputBox/InputBox";
