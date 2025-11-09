fee4bc509f84523a296d89da6c5d08680e2a13a1
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-10 08:46:00 +0900

    나중에 없앨 커밋
    "

--- src/common/Button/FloatingButton.tsx
+++ src/common/Button/FloatingButton.tsx
@@ -1,5 +1,21 @@
-const FloatingButton = () => {
-  return <div>FloatingButton</div>;
+import { cn } from "@/utils";
+import { ReactNode } from "react";
+
+export const FLOATING_BUTTON_BASE_STYLE = "size-14 rounded-full py-[14px] px-[21px] flex-center";
+export const FLOATING_BUTTON_STYLE_BY_VARIANT = {
+  primary: "bg-brand-primary",
+  inverse: "bg-background-inverse border border-border-primary shadow-xl",
+};
+
+type Variant = keyof typeof FLOATING_BUTTON_STYLE_BY_VARIANT;
+
+interface FloatingButtonProps {
+  variant: Variant;
+  icon: ReactNode;
+}
+
+const FloatingButton = ({ variant, icon }: FloatingButtonProps) => {
+  return <button className={cn(FLOATING_BUTTON_BASE_STYLE, FLOATING_BUTTON_STYLE_BY_VARIANT[variant])}>d</button>;
 };
 
 export default FloatingButton;
