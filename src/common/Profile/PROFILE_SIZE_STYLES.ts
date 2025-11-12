eb664b0371dc35db5bdc3dead9837aee389b56af
Kim_Won_Seon <gws8221@gmail.com>
2025-11-12 23:12:39 +0900

    design: size 상수 추가

---
+++ src/common/Profile/PROFILE_SIZE_STYLES.ts
@@ -0,0 +1,12 @@
+// 프로필 컴포넌트
+export const PROFILE_SIZE = {
+  sm: "size-6 rounded-[6px]",
+  md: "size-8 rounded-[8px]",
+  lg: "size-10 rounded-[12px]",
+} as const;
+
+export const PROFILE_IMAGE_SIZE = {
+  sm: 24,
+  md: 32,
+  lg: 40,
+} as const;
