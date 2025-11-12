1bc7bb87a1adc1e9e0c63ccf70c8ba3c2f583e52
Kim_Won_Seon <gws8221@gmail.com>
2025-11-12 23:01:31 +0900

    feat: 프로필 컴포넌트 구현

---
+++ src/common/Profile/Profile.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { cn } from "@/utils";
+import { Icon } from "@/types";
+import { PROFILE_SIZE, PROFILE_IMAGE_SIZE } from "./PROFILE_SIZE_STYLES";
+
+/**
+ * @author KimWonSeon
+ * @description 프로필 컴포넌트입니다.
+ *
+ * @param src - 이미지 URL, 없을 시 기본 이미지
+ * @param alt - 이미지 대체 텍스트
+ * @param size - sm, md, lg 프로필 크기 옵션
+ */
+
+export interface ProfileProps {
+  src: string | null;
+  alt?: string;
+  size?: "sm" | "md" | "lg";
+}
+
+const Profile = ({ src, alt = "프로필", size = "lg" }: ProfileProps) => {
+  return (
+    <div className={cn("overflow-hidden bg-background-tertiary flexCenter flex-shrink-0", PROFILE_SIZE[size])}>
+      {src ? (
+        <Image
+          src={src}
+          alt={alt}
+          width={PROFILE_IMAGE_SIZE[size]}
+          height={PROFILE_IMAGE_SIZE[size]}
+          className={cn("w-full h-full object-cover")}
+        />
+      ) : (
+        // TODO(김원선): Icon 사이즈 조정 가능시 수정
+        <Icon name="humanLg" size="md" className={cn("flexCenter")} />
+      )}
+    </div>
+  );
+};
+
+export default Profile;
