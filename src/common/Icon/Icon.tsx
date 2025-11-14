fc77a15e7c7599ba835d3379f2cef76b11fa9a25
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-14 20:46:49 +0900

    fix: 버튼에서 사용하는 아이콘 수정

--- src/common/Icon/Icon.tsx
+++ src/common/Icon/Icon.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/utils";
 import ICONS from "./CONST_ICON";
+import { SVGProps } from "react";
 
-type IconKeys = keyof typeof ICONS;
+export type IconKeys = keyof typeof ICONS;
 
-interface IconProps {
+interface IconProps extends SVGProps<SVGSVGElement> {
   name: IconKeys;
   className?: string;
 }
@@ -13,6 +14,7 @@ interface IconProps {
  * @example
  * <Icon name="alert"/>
  * <Icon name="alert" className="text-primary"/>
+ * <Icon name="alert" className="text-primary" onClick={}/>
  */
 const Icon = ({ name, className }: IconProps) => {
   const SvgIcon = ICONS[name];
