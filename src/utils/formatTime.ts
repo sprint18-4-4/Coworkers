a974b94d3e1997237018d894c65d165c892524c4
Kim_Won_Seon <gws8221@gmail.com>
2025-11-15 04:12:03 +0900

    refactor: TsDoc 추가 및 중복 코드 삭제

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,11 +1,36 @@
+/**
+ * @author KimWonSeon
+ * @description 상대 시간을 문자열로 변환
+ *
+ * @remarks
+ * - 1분 미만: "방금 전"
+ * - 1시간 미만: "n분 전"
+ * - 24시간 미만: "n시간 전"
+ * - 7일 미만: "n일 전"
+ * - 7일 이상: "YYYY. MM. DD"
+ *
+ *  * @example
+ *
+ * formatRelativeTime("2025-11-14T12:58:00Z") 등록
+ * // "방금 전"
+ *
+ * formatRelativeTime("2025-11-14T12:30:00Z")
+ * // "30분 전"
+ *
+ * formatRelativeTime("2025-11-14T09:00:00Z")
+ * // "4시간 전"
+ *
+ * formatRelativeTime("2025-11-12T12:00:00Z")
+ * // "2일 전"
+ *
+ * formatRelativeTime("2025-11-25T00:00:00Z")
+ * // "2025. 11. 15"
+ */
+
 export const formatTime = (dateString: string): string => {
   const now = new Date();
   const date = new Date(dateString);
   const diffInSeconds = Math.floor(now.getTime() - date.getTime() / 1000);
-
-  if (diffInSeconds < 0) {
-    return "방금 전";
-  }
 
   if (diffInSeconds < 60) {
     return "방금 전";
