5ed5411c24e109d311c13b0adee0724b42c59378
Kim_Won_Seon <gws8221@gmail.com>
2025-11-15 05:35:54 +0900

    feat: Toast 컴포넌트 구현

---
+++ src/common/Toast/Toast.tsx
@@ -0,0 +1,30 @@
+import { BaseButton, Icon } from "@/common";
+
+interface ToastProps {
+  show: boolean;
+  message: string;
+  onSave: () => void;
+}
+
+const UnsavedChangesToast = ({ show, message, onSave }: ToastProps) => {
+  if (!show) return null;
+
+  return (
+    <div className="max-w-[868px] min-w-[343px] min-h-[49px] max-h-[57px] bg-brand-primary px-3 py-3 rounded-lg flex items-center gap-2.5">
+      <span className="flex items-center gap-2 flex-grow text-md-semibold text-text-inverse tablet:text-lg-semibold">
+        <Icon name="alert" className="hidden tablet:block tablet:size-6" />
+        {message}
+      </span>
+      <BaseButton
+        variant="solid"
+        size="small"
+        onClick={onSave}
+        className="w-[125px] h-[33px] tablet:w-[141px] bg-background-inverse text-md-semibold hover:bg-background-secondary"
+      >
+        <span className="text-brand-primary">변경사항 저장하기</span>
+      </BaseButton>
+    </div>
+  );
+};
+
+export default UnsavedChangesToast;
