04dc29e011a347e373c48544d31db9ec0f72ca1b
Kim_Won_Seon <gws8221@gmail.com>
2025-11-15 17:58:59 +0900

    refactor: 타입 수정

--- src/common/Comment/_types/type.ts
+++ src/common/Comment/_types/type.ts
@@ -3,17 +3,17 @@ import { CommentData } from "@/types";
 export interface CommentProps {
   comment: CommentData;
   showKebab?: boolean;
-  isEditing?: boolean;
-  onEdit?: () => void;
-  onDelete?: () => void;
-  onSave?: (content: string) => void;
-  onCancel?: () => void;
+  onEditClose?: () => void;
   className?: string;
 }
 
 export interface CommentEditProps {
-  initialContent: string;
-  onSave: (content: string) => void;
-  onCancel: () => void;
+  initialComment: string;
+  onClose: () => void;
   className?: string;
+}
+
+export interface KebabMenuProps {
+  commentId: number;
+  onEdit: () => void;
 }
