86d2e6896e74d4dc6d1218b94881e6562ee6c4a4
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-17 05:48:06 +0900

    feat: dropdown 구현

---
+++ src/common/Dropdown/Dropdown.tsx
@@ -0,0 +1,100 @@
+import { ReactNode, useState } from "react";
+import { useDropdown } from "@/hooks";
+import { DropdownContext } from "@/hooks";
+import { cn } from "@/utils";
+
+interface DropdownType {
+  children: ReactNode;
+  className?: string;
+}
+
+interface DropdownItemType extends DropdownType {
+  onClick: () => void;
+}
+
+const Dropdown = ({ children }: DropdownType) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <DropdownContext.Provider value={{ isOpen, onToggle, onClose }}>
+      <div className="relative inline-block">{children}</div>
+    </DropdownContext.Provider>
+  );
+};
+
+const DropdownToggle = ({ children, className }: DropdownType) => {
+  const { onToggle } = useDropdown();
+
+  return (
+    <button className={cn(className)} onClick={onToggle}>
+      {children}
+    </button>
+  );
+};
+
+const DropdownItems = ({ children, className }: DropdownType) => {
+  const { isOpen } = useDropdown();
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <ul
+      className={cn(
+        "w-full flex flex-col-center border border-border-primary rounded-xl absolute top-full bg-background-primary",
+        className,
+      )}
+    >
+      {children}
+    </ul>
+  );
+};
+
+const DropdownItem = ({ children, className, onClick }: DropdownItemType) => {
+  const { onClose } = useDropdown();
+  const handleButtonClick = () => {
+    onClick();
+    onClose();
+  };
+
+  return (
+    <li className="w-full">
+      <button className={cn("w-full p-[14px]", className)} onClick={handleButtonClick}>
+        {children}
+      </button>
+    </li>
+  );
+};
+
+Dropdown.Toggle = DropdownToggle;
+Dropdown.Items = DropdownItems;
+Dropdown.Item = DropdownItem;
+export default Dropdown;
+
+// <Dropdown>
+//   <Dropdown.Toggle className="flex gap-2 px-[14px] py-[10px] bg-background-primary rounded-xl">
+//     <span>{selected}</span>
+//     <Icon name="downArrow" />
+//   </Dropdown.Toggle>
+//   <Dropdown.Items className="">
+//     <Dropdown.Item onClick={() => setSelected("최신순")}>최신순</Dropdown.Item>
+//     <Dropdown.Item onClick={() => setSelected("조회순")}>조회순</Dropdown.Item>
+//   </Dropdown.Items>
+// </Dropdown>
+// <Dropdown>
+//   <Dropdown.Toggle>
+//     <Icon name="kebab" />
+//   </Dropdown.Toggle>
+//   <Dropdown.Items className="w-[120px]">
+//     <Dropdown.Item onClick={() => []}>수정하기</Dropdown.Item>
+//     <Dropdown.Item onClick={() => {}}>삭제하기</Dropdown.Item>
+//   </Dropdown.Items>
+// </Dropdown>
