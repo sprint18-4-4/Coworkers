e92524b5f40a2945a37dcad313a25a79e34ccc44
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-17 06:49:52 +0900

    chore: tsDoc 추가

--- src/common/Dropdown/Dropdown.tsx
+++ src/common/Dropdown/Dropdown.tsx
@@ -3,6 +3,39 @@ import { useDropdown } from "@/hooks";
 import { DropdownContext } from "@/hooks";
 import { cn } from "@/utils";
 
+/**
+ * @author sangin
+ *
+ * @example
+ * 기본 정렬 드롭다운
+ * ```tsx
+ * <Dropdown>
+ *   <Dropdown.Toggle className="flex gap-2 px-[14px] py-[10px] bg-background-primary rounded-xl">
+ *     <span>{sorted}</span>
+ *     <Icon name="downArrow" />
+ *   </Dropdown.Toggle>
+ *   <Dropdown.Items className="">
+ *     <Dropdown.Item onClick={() => setSorted("최신순")}>최신순</Dropdown.Item>
+ *     <Dropdown.Item onClick={() => setSorted("조회순")}>조회순</Dropdown.Item>
+ *   </Dropdown.Items>
+ * </Dropdown>
+ * ```
+ *
+ * @example
+ * 케밥 메뉴 드롭다운
+ * ```tsx
+ * <Dropdown>
+ *   <Dropdown.Toggle>
+ *     <Icon name="kebab" />
+ *   </Dropdown.Toggle>
+ *   <Dropdown.Items className="w-[120px]">
+ *     <Dropdown.Item onClick={() => console.log('수정')}>수정하기</Dropdown.Item>
+ *     <Dropdown.Item onClick={() => console.log('삭제')}>삭제하기</Dropdown.Item>
+ *   </Dropdown.Items>
+ * </Dropdown>
+ * ```
+ */
+
 interface DropdownType {
   children: ReactNode;
   className?: string;
