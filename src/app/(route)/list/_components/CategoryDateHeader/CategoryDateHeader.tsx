68e7414c64ecae4a51193a95c4dccc2becca7bda
wlrnjs <wlrnjs5824@gmail.com>
2025-11-18 00:51:56 +0900

    feat: FloatingButton 추가

--- src/app/(route)/list/_components/CategoryDateHeader/CategoryDateHeader.tsx
+++ src/app/(route)/list/_components/CategoryDateHeader/CategoryDateHeader.tsx
@@ -1,25 +1,43 @@
-import { Icon } from "@/common";
+import { DateItem, Icon } from "@/common";
+import { DateNumber, Day } from "@/common/DateItem/_types";
+
+const DATE_MOCK: { day: Day; date: DateNumber }[] = [
+  { day: "월", date: 1 },
+  { day: "화", date: 2 },
+  { day: "수", date: 3 },
+  { day: "목", date: 4 },
+  { day: "금", date: 5 },
+  { day: "토", date: 6 },
+  { day: "일", date: 7 },
+];
 
 const CategoryDateHeader = () => {
   return (
-    <div className="flex items-center justify-between">
-      <h3 className="text-2lg-bold text-text-primary">법인 등기</h3>
+    <>
+      <div className="flex items-center justify-between">
+        <h3 className="text-2lg-bold text-text-primary">법인 등기</h3>
 
-      <div className="flex items-center gap-2">
-        <span className="text-sm-medium text-text-primary">2025년 5월</span>
-        <div className="flex items-center gap-1">
-          <button className="size-4 rounded-full border border-slate-200 flex-center">
-            <Icon name="leftArrow" className="size-3 tablet:size-3" />
-          </button>
-          <button className="size-4 rounded-full border border-slate-200 flex-center">
-            <Icon name="rightArrow" className="size-3 tablet:size-3" />
+        <div className="flex items-center gap-2">
+          <span className="text-sm-medium text-text-primary">2025년 5월</span>
+          <div className="flex items-center gap-1">
+            <button className="size-4 rounded-full border border-slate-200 flex-center">
+              <Icon name="leftArrow" className="size-3 tablet:size-3" />
+            </button>
+            <button className="size-4 rounded-full border border-slate-200 flex-center">
+              <Icon name="rightArrow" className="size-3 tablet:size-3" />
+            </button>
+          </div>
+          <button className="size-6 rounded-full bg-background-secondary flex-center">
+            <Icon name="calendar" className="size-3 tablet:size-3" />
           </button>
         </div>
-        <button className="size-6 rounded-full bg-background-secondary flex-center">
-          <Icon name="calendar" className="size-3 tablet:size-3" />
-        </button>
       </div>
-    </div>
+      <div className="flex items-center gap-1 mt-6">
+        {DATE_MOCK.map((day) => {
+          return <DateItem key={day.day} day={day.day} date={day.date} />;
+        })}
+      </div>
+    </>
   );
 };
 
--- src/app/(route)/list/page.tsx
+++ src/app/(route)/list/page.tsx
@@ -1,43 +1,44 @@
-import { DateItem, PageHeaderBar } from "@/common";
-import { Day, DateNumber } from "@/common/DateItem/_types";
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+// TODO(지권): 머지 후 PageLayout import 수정 필요
 import PageLayout from "@/common/PageLayout/PageLayout";
+import { FloatingButton, PageHeaderBar } from "@/common";
 import { CategoryDateHeader, TodoSectionHeader } from "./_components";
 
-const DATE_MOCK: { day: Day; date: DateNumber }[] = [
-  { day: "월", date: 1 },
-  { day: "화", date: 2 },
-  { day: "수", date: 3 },
-  { day: "목", date: 4 },
-  { day: "금", date: 5 },
-  { day: "토", date: 6 },
-  { day: "일", date: 7 },
-];
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-const page = () => {
+  const onClickFloatingButton = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("w", "true");
+
+    router.push(`/list?${params.toString()}`);
+  };
+
   return (
-    <PageLayout ariaLabel="목록 페이지">
-      <h1 className="sr-only">목록 페이지</h1>
-      <PageHeaderBar title="경영관리팀" />
+    <>
+      <PageLayout ariaLabel="목록 페이지">
+        <h1 className="sr-only">목록 페이지</h1>
+        <PageHeaderBar title="경영관리팀" />
 
-      <div aria-label="목록 페이지 컨텐츠" className="pc:flex pc:gap-[86px]">
-        <TodoSectionHeader />
+        <div aria-label="목록 페이지 컨텐츠" className="pc:flex pc:gap-[86px]">
+          <TodoSectionHeader />
 
-        <div className="bg-background-primary px-[17px] py-[38px] mt-[22px]">
-          <CategoryDateHeader />
+          <div className="bg-background-primary px-[17px] py-[38px] mt-[22px]">
+            <CategoryDateHeader />
 
-          <div className="flex items-center gap-1 mt-6">
-            {DATE_MOCK.map((day) => {
-              return <DateItem key={day.day} day={day.day} date={day.date} />;
-            })}
-          </div>
-
-          <div className="mt-[37px]">
-            <h4>asdasdasd</h4>
+            <div className="mt-[37px]">
+              <h4>목록 아이템이 들어갈 영역입니다</h4>
+            </div>
           </div>
         </div>
-      </div>
-    </PageLayout>
+      </PageLayout>
+
+      <FloatingButton iconName="plus" className="absolute bottom-2 right-2" onClick={onClickFloatingButton} />
+    </>
   );
 };
 
-export default page;
+export default Page;
