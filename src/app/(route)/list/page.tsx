cf1bb44a5a7f156c44cfe4eace5961e8782956e5
wlrnjs <wlrnjs5824@gmail.com>
2025-11-17 17:23:22 +0900

    chore: 임시커밋

---
+++ src/app/(route)/list/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "@/common/PageLayout/PageLayout";
+
+const page = () => {
+  return (
+    <PageLayout ariaLabel="목록 페이지">
+      <h1>목록 페이지</h1>
+    </PageLayout>
+  );
+};
+
+export default page;
