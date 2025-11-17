ca9296285c014b2ed3538bb04d3606a2807ee9c7
wlrnjs <wlrnjs5824@gmail.com>
2025-11-17 17:04:50 +0900

    refactor: PageLayout 분리 및 TSDoc 추가

--- src/app/(route)/my-history/page.tsx
+++ src/app/(route)/my-history/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { cn } from "@/utils";
 import { PageHeaderBar } from "@/common";
 import { MyWorkHistory, WorkHistorySection } from "./_components";
 import { MY_HISTORY_ITEM_MOCK_DATA } from "@/MOCK_DATA";
+import PageLayout from "@/common/PageLayout/PageLayout";
 
 const page = () => {
   return (
-    <div
-      aria-label="나의 히스토리"
-      className={cn(
-        "min-h-[calc(100vh-52px)] px-4 pt-[17px] flex flex-col gap-[25px] bg-background-secondary",
-        "tablet:min-h-[100vh] tablet:px-[26px] tablet:pt-[69px]",
-        "pc:mx-0 pc:pl-[91px] pc:pt-[90px] pc:gap-12",
-      )}
-    >
+    <PageLayout ariaLabel="나의 히스토리">
       <h1 className="sr-only">나의 히스토리</h1>
       <PageHeaderBar title="경영관리팀" />
 
@@ -22,7 +15,7 @@ const page = () => {
         <MyWorkHistory title="나의 히스토리" items={MY_HISTORY_ITEM_MOCK_DATA.map((item) => ({ item }))} />
         <WorkHistorySection />
       </div>
-    </div>
+    </PageLayout>
   );
 };
 
