6c5b683afe0cd1f16832dfccd0b9382ed8c29f04
wlrnjs <wlrnjs5824@gmail.com>
2025-11-17 18:23:19 +0900

    chore: 임시 커밋

--- src/app/(route)/list/page.tsx
+++ src/app/(route)/list/page.tsx
@@ -1,9 +1,14 @@
+import { PageHeaderBar } from "@/common";
 import PageLayout from "@/common/PageLayout/PageLayout";
 
 const page = () => {
   return (
     <PageLayout ariaLabel="목록 페이지">
-      <h1>목록 페이지</h1>
+      <h1 className="sr-only">목록 페이지</h1>
+      <PageHeaderBar title="경영관리팀" />
+      <div aria-label="목록 페이지 컨텐츠" className="pc:flex pc:gap-[86px]">
+        <h2>ㄴㅇㅁㄴㅇ</h2>
+      </div>
     </PageLayout>
   );
 };
