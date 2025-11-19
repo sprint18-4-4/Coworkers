48ac8faec13eb213cc7d7e2f1dca7557b1ea6a70
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-20 07:46:11 +0900

    feat: MemberWidget 구조 만들기

--- src/app/(route)/team/_components/MemberWidget/MemberWidget.tsx
+++ src/app/(route)/team/_components/MemberWidget/MemberWidget.tsx
@@ -1,5 +1,38 @@
+import { Icon, ProfileItem } from "@/common";
+
+// GET: /teamId/groups/id
 const MemberWidget = () => {
-  return <div>MemberWidget</div>;
+  return (
+    <aside className="flex flex-col gap-6 w-[240px] border border-border-primary rounded-xl bg-background-primary px-5 py-[24px]">
+      <header className="flex justify-between items-center">
+        <span className="flex gap-2">
+          <span className="text-lg-medium text-text-primary">멤버</span>
+          <span className="text-lg-regular text-text-default">(4명)</span>
+        </span>
+
+        {/* TODO(상인): 모달 머지될 시 띄우기 */}
+        <button onClick={() => {}} className="flex items-center gap-1 text-lg-semibold text-brand-primary">
+          <span>초대하기</span>
+          <Icon name="plus" className="size-4 tablet:size-4" />
+        </button>
+      </header>
+
+      <ul className="flex flex-col gap-[18px]">
+        <li>
+          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
+        </li>
+        <li>
+          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
+        </li>
+        <li>
+          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
+        </li>
+        <li>
+          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
+        </li>
+      </ul>
+    </aside>
+  );
 };
 
 export default MemberWidget;
--- src/app/(route)/team/page.tsx
+++ src/app/(route)/team/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PageLayout } from "@/common";
 import { MemberWidget, ProgressWidget, TaskList } from "./_components";
 
