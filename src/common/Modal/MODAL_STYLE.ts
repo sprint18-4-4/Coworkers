5414857d8b6b17755032df8a2a3cb9006f4f65c4
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-19 19:25:16 +0900

    refactor: Modal 구조 변경

--- src/common/Modal/MODAL_STYLE.ts
+++ src/common/Modal/MODAL_STYLE.ts
@@ -1,9 +1,6 @@
 export const MODAL_BASE_STYLE =
   "w-[375px] rounded-[24px] rounded-b-none tablet:w-[384px] tablet:rounded-b-[24px] relative bg-background-primary flex-col-center p-8";
 export const MODAL_OVERLAY_STYLE = "fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm";
-
-export const MODAL_TITLE_STYLE = "text-text-primary text-lg-medium mb-2";
-export const MODAL_DESCRIPTION_STYLE = "text-md-medium mb-2";
-export const MODAL_BUTTON_STYLE = "flex flex-1 gap-2 w-full mt-4";
-export const MODAL_ICON_STYLE = "mb-3";
 export const MODAL_CLOSE_ICON_STYLE = "absolute right-4 top-2 flex-center cursor-pointer text-icon-primary";
+export const MODAL_BODY_STYLE = "py-4 w-full";
+export const MODAL_FOOTER_STYLE = "flex flex-1 gap-2 w-full";
--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,237 +1,67 @@
 "use client";
 
+import { MouseEvent, ReactNode, useEffect } from "react";
+import { cn } from "@/utils";
 import {
   MODAL_BASE_STYLE,
-  MODAL_OVERLAY_STYLE,
+  MODAL_BODY_STYLE,
   MODAL_CLOSE_ICON_STYLE,
-  MODAL_TITLE_STYLE,
-  MODAL_DESCRIPTION_STYLE,
-  MODAL_BUTTON_STYLE,
-  MODAL_ICON_STYLE,
+  MODAL_FOOTER_STYLE,
+  MODAL_OVERLAY_STYLE,
 } from "./MODAL_STYLE";
-import {
-  ModalProps,
-  ModalTitleProps,
-  ModalDescriptionProps,
-  ModalButtonProps,
-  ModalIconProps,
-  ModalFormProps,
-  ModalInputProps,
-  ModalCloseIconProps,
-  ModalTimeProps,
-  Panel,
-} from "./_types/ModalProps";
-import { cn } from "@/utils";
 import Icon from "../Icon/Icon";
-import Input from "../Input/Input";
-import Time from "../Calendar/Time/Time";
-import DatePicker from "../Calendar/DatePicker/DatePicker";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import Profile from "../Profile/Profile";
-import BaseButton from "../Button/BaseButton";
-import { ModalProfileProps } from "./_types/ModalProps";
-/**
- *  @author sangin
- *  @component
- *  @example
- *
- * @param onClose onClose는 setIsOpen만 전달해주면 Modal에서 처리하도록 했습니다.
- * <Modal isOpen={isOpen} onClose={setIsOpen} className="">
- *   <Modal.Title title="제목" />
- *   <Modal.Description description="설명" />
- * </Modal>
- */
 
-const Modal = ({ isOpen, children, className, onClose }: ModalProps) => {
-  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  className?: string;
+  children: ReactNode;
+}
+
+interface ModalContentProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Modal = ({ isOpen, onClose, className, children }: ModalProps) => {
+  const handleOverlayClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) onClose();
   };
 
   useEffect(() => {
-    const handleEsc = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        onClose();
-      }
-    };
+    const handleEsc = (e: KeyboardEvent) => e.key === "Escape" && onClose();
+
     document.addEventListener("keydown", handleEsc);
-    return () => {
-      document.removeEventListener("keydown", handleEsc);
-    };
+    return () => document.removeEventListener("keydown", handleEsc);
   }, [onClose]);
 
+  if (!isOpen) return null;
+
   return (
-    isOpen && (
-      <div className={MODAL_OVERLAY_STYLE} onClick={handleOverlayClick}>
-        <div className={cn(MODAL_BASE_STYLE, className)}>{children}</div>
-      </div>
-    )
+    <div className={MODAL_OVERLAY_STYLE} onClick={handleOverlayClick}>
+      <div className={cn(MODAL_BASE_STYLE, className)}>{children}</div>
+    </div>
   );
 };
 
-const ModalTitle = ({ title, className }: ModalTitleProps) => {
-  return <h2 className={cn(MODAL_TITLE_STYLE, className)}>{title}</h2>;
-};
-
-const ModalDescription = ({ description, className }: ModalDescriptionProps) => {
-  return <p className={cn(MODAL_DESCRIPTION_STYLE, className)}>{description}</p>;
-};
-
-const ModalButton = ({ children, className }: ModalButtonProps) => {
-  return <div className={cn(MODAL_BUTTON_STYLE, className)}>{children}</div>;
-};
-
-const ModalIcon = ({ name, className }: ModalIconProps) => {
-  return <Icon name={name} className={cn(MODAL_ICON_STYLE, className)} />;
-};
-
-const ModalCloseIcon = ({ className, onClose }: ModalCloseIconProps) => {
+const ModalCloseIcon = ({ className, onClose }: { className?: string; onClose: () => void }) => {
   return (
-    <button onClick={() => onClose(false)}>
+    <button onClick={onClose}>
       <Icon name="x" className={cn(MODAL_CLOSE_ICON_STYLE, className)} />
     </button>
   );
 };
 
-const ModalForm = ({ children, onSubmit }: ModalFormProps) => {
-  return <form onSubmit={onSubmit}>{children}</form>;
+const ModalBody = ({ children, className }: ModalContentProps) => {
+  return <div className={cn(MODAL_BODY_STYLE, className)}>{children}</div>;
 };
 
-const ModalInput = ({ placeholder, label, error, containerClassName, ref, ...rest }: ModalInputProps) => {
-  return (
-    <Input
-      placeholder={placeholder}
-      label={label}
-      error={error}
-      containerClassName={containerClassName}
-      ref={ref}
-      {...rest}
-    />
-  );
+const ModalFooter = ({ children, className }: ModalContentProps) => {
+  return <div className={cn(MODAL_FOOTER_STYLE, className)}>{children}</div>;
 };
 
-// TODO(상인): 토스트 메시지가 생기면 클립보드 복사 + 토스트메시지 onClick 구현
-const ModalProfile = ({ src, alt, size, name, email }: ModalProfileProps) => {
-  return (
-    <div className="w-full">
-      <div className="flex-col-center gap-4">
-        <Profile src={src} alt={alt} size={size} />
-        <div className="flex-col-center">
-          <span>{name}</span>
-          <span>{email}</span>
-        </div>
-      </div>
-
-      <Modal.Button>
-        <BaseButton variant="solid" size="large" onClick={() => {}}>
-          이메일 복사하기
-        </BaseButton>
-      </Modal.Button>
-    </div>
-  );
-};
-
-// TODO(상인): Dropdown이 구현되면 수정
-const ModalRepeat = ({}) => {
-  // return <Dropdown/>
-};
-
-const ModalTime = ({ value, onChange, timePeriod, setTimePeriod, selectedTime, setSelectedTime }: ModalTimeProps) => {
-  const [activePanel, setActivePanel] = useState<Panel | null>(null);
-  const [isPanelOpen, setIsPanelOpen] = useState(false);
-
-  const handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
-    const panel = e.currentTarget.name as Panel;
-    if (activePanel === panel && isPanelOpen) {
-      setIsPanelOpen(false);
-      setActivePanel(null);
-      return;
-    }
-    setActivePanel(panel);
-    setIsPanelOpen(true);
-  };
-
-  const closePanel = () => {
-    setIsPanelOpen(false);
-    setActivePanel(null);
-  };
-
-  const formatDate = () => {
-    if (Array.isArray(value)) {
-      const [start, end] = value;
-      return `${start ? dayjs(start).format("YYYY년 MM월 DD일") : ""}${
-        end ? ` ~ ${dayjs(end).format("YYYY년 MM월 DD일")}` : ""
-      }`;
-    }
-    return value ? dayjs(value).format("YYYY년 MM월 DD일") : "";
-  };
-
-  const formatTime = (timePeriod: "am" | "pm", selectedTime: `${number}:00` | `${number}:30`) => {
-    if (timePeriod === "am") return `오전 ${selectedTime}`;
-    return `오후 ${selectedTime}`;
-  };
-
-  return (
-    <div className="flex flex-col gap-4">
-      <label className="text-lg-medium">시작 날짜 및 시간</label>
-      <div className="flex gap-2">
-        <Input
-          name="DatePicker"
-          readOnly
-          onClick={handleInputClick}
-          value={formatDate()}
-          containerClassName="flex-[3]"
-          className="cursor-pointer caret-transparent"
-        />
-        <Input
-          name="Time"
-          readOnly
-          onClick={handleInputClick}
-          value={formatTime(timePeriod, selectedTime)}
-          containerClassName="flex-[2]"
-          className="cursor-pointer caret-transparent"
-        />
-      </div>
-
-      {isPanelOpen && activePanel === "DatePicker" && (
-        <DatePicker
-          value={value}
-          onChange={(v) => {
-            onChange(v);
-            closePanel();
-          }}
-        />
-      )}
-
-      {isPanelOpen && activePanel === "Time" && (
-        <Time
-          timePeriod={timePeriod}
-          setTimePeriod={(v) => {
-            setTimePeriod(v);
-            closePanel();
-          }}
-          selectedTime={selectedTime}
-          setSelectedTime={(v) => {
-            setSelectedTime(v);
-            closePanel();
-          }}
-        />
-      )}
-    </div>
-  );
-};
-
-Modal.Title = ModalTitle;
-Modal.Button = ModalButton;
-Modal.Form = ModalForm;
-Modal.Input = ModalInput;
-Modal.Description = ModalDescription;
-Modal.Icon = ModalIcon;
 Modal.CloseIcon = ModalCloseIcon;
-Modal.Profile = ModalProfile;
-Modal.Repeat = ModalRepeat;
-Modal.Time = ModalTime;
+Modal.Body = ModalBody;
+Modal.Footer = ModalFooter;
 
 export default Modal;
