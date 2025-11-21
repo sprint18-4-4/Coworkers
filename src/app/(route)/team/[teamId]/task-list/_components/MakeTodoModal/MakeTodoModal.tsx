cdc070b3fa305b89ed43f24986ce309427cbc257
wlrnjs <wlrnjs5824@gmail.com>
2025-11-21 19:14:17 +0900

    chore: 최신화 커밋

--- src/app/(route)/team/[teamId]/task-list/_components/MakeTodoModal/MakeTodoModal.tsx
+++ src/app/(route)/team/[teamId]/task-list/_components/MakeTodoModal/MakeTodoModal.tsx
@@ -22,68 +22,97 @@ const MakeTodoModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => vo
   const [order, setOrder] = useState("once");
   const [startTime, setStartTime] = useState(new Date());
   const [openPicker, setOpenPicker] = useState<OpenPicker>(null);
+  const [value, setValue] = useState({
+    title: "",
+    startDate: new Date(),
+    startTime: new Date(),
+    order: "once",
+    todoMemo: "",
+  });
+
+  const isFormValid =
+    value.title.trim().length > 0 &&
+    value.todoMemo.trim().length > 0 &&
+    value.startDate !== null &&
+    value.startTime !== null &&
+    value.order !== "";
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(value);
+    onClose();
+  };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <Modal.Body className="flex-col-center gap-4">
-        <h2 className="text-text-primary text-lg-medium">할 일 만들기</h2>
-        <p className="text-text-default text-md-medium text-center">
-          할 일은 실제로 행동 가능한 작업 중심으로 <br /> 작성해주시면 좋습니다.
-        </p>
+    <form onSubmit={onSubmit}>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal.Body className="flex-col-center gap-4">
+          <h2 className="text-text-primary text-lg-medium">할 일 만들기</h2>
+          <p className="text-text-default text-md-medium text-center">
+            할 일은 실제로 행동 가능한 작업 중심으로 <br /> 작성해주시면 좋습니다.
+          </p>
 
-        <Input className="" label="할 일 제목" placeholder="할 일 제목을 입력해주세요." />
-        <div className={STYLES.baseDiv}>
-          <p>시작 날짜 및 시간</p>
-          <div className="w-full flex gap-2">
-            <div className="flex-[2]">
-              <Input
-                placeholder="2024년 7월 29일"
-                className={cn(
-                  "w-full cursor-pointer focus:outline-none",
-                  openPicker === "date" && "border-interaction-pressed",
-                )}
-                readOnly
-                onClick={() => setOpenPicker((prev) => (prev === "date" ? null : "date"))}
-              />
+          <Input
+            value={value.title}
+            label="할 일 제목"
+            placeholder="할 일 제목을 입력해주세요."
+            onChange={(e) => setValue((prev) => ({ ...prev, title: e.target.value }))}
+          />
+          <div className={cn(STYLES.baseDiv, "-mb-4")}>
+            <p>시작 날짜 및 시간</p>
+            <div className="w-full flex gap-2">
+              <div className="flex-[2]">
+                <Input
+                  placeholder="2024년 7월 29일"
+                  className={cn(
+                    "w-full cursor-pointer focus:outline-none",
+                    openPicker === "date" && "border-interaction-pressed",
+                  )}
+                  readOnly
+                  onClick={() => setOpenPicker((prev) => (prev === "date" ? null : "date"))}
+                />
+              </div>
+              <div className="flex-[1]">
+                <Input
+                  placeholder="오후 3:30"
+                  className={cn(
+                    "w-full cursor-pointer focus:outline-none",
+                    openPicker === "time" && "border-interaction-pressed",
+                  )}
+                  readOnly
+                  onClick={() => setOpenPicker((prev) => (prev === "time" ? null : "time"))}
+                />
+              </div>
             </div>
-            <div className="flex-[1]">
-              <Input
-                placeholder="오후 3:30"
-                className={cn(
-                  "w-full cursor-pointer focus:outline-none",
-                  openPicker === "time" && "border-interaction-pressed",
-                )}
-                readOnly
-                onClick={() => setOpenPicker((prev) => (prev === "time" ? null : "time"))}
-              />
-            </div>
+            <AnimatedCollapse isOpen={openPicker === "date"}>
+              <DatePicker value={startTime} onChange={() => {}} />
+            </AnimatedCollapse>
+            <AnimatedCollapse isOpen={openPicker === "time"}>
+              <Time timePeriod="am" setTimePeriod={() => {}} selectedTime="12:00" setSelectedTime={() => {}} />
+            </AnimatedCollapse>
           </div>
-          <AnimatedCollapse isOpen={openPicker === "date"}>
-            <DatePicker value={startTime} onChange={() => {}} />
-          </AnimatedCollapse>
-          <AnimatedCollapse isOpen={openPicker === "time"}>
-            <Time timePeriod="am" setTimePeriod={() => {}} selectedTime="12:00" setSelectedTime={() => {}} />
-          </AnimatedCollapse>
-        </div>
 
-        <div className={STYLES.baseDiv}>
-          <p>반복 설정</p>
-          <Select value={order} options={options} onChange={setOrder} />
-        </div>
+          <div className={STYLES.baseDiv} onClick={(e) => e.preventDefault()}>
+            <p>반복 설정</p>
+            <Select value={order} options={options} onChange={setOrder} />
+          </div>
 
-        <InputBox
-          size="md"
-          label="할 일 메모"
-          labelClassName="!text-lg-medium"
-          placeholder="할 일 메모를 입력해주세요."
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        <BaseButton variant="solid" size="large" onClick={onClose} className="mt-4">
-          만들기
-        </BaseButton>
-      </Modal.Footer>
-    </Modal>
+          <InputBox
+            value={value.todoMemo}
+            onChange={(e) => setValue((prev) => ({ ...prev, todoMemo: e.target.value }))}
+            size="md"
+            label="할 일 메모"
+            labelClassName="!text-lg-medium"
+            placeholder="할 일 메모를 입력해주세요."
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <BaseButton type="submit" variant="solid" size="large" className="mt-4" disabled={!isFormValid}>
+            만들기
+          </BaseButton>
+        </Modal.Footer>
+      </Modal>
+    </form>
   );
 };
 
