4c365e003efb4edc7fb64ac686c07d8bdeb093a0
wlrnjs <wlrnjs5824@gmail.com>
2025-11-22 06:46:09 +0900

    chore: 댓글 Submit 임시 커밋

--- src/app/(route)/team/[teamId]/task-list/_detail/_components/_internal/CommentSection/CommentSection.tsx
+++ src/app/(route)/team/[teamId]/task-list/_detail/_components/_internal/CommentSection/CommentSection.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { cn } from "@/utils";
 import { DetailDataItem } from "@/types";
 import { CommentItem, InputReply, Profile } from "@/common";
 import { TASK_DETAIL_COMMENT_MOCK_DATA } from "@/MOCK_DATA";
+import { FormEvent } from "react";
 
 interface CommentSectionProps {
   id: string;
@@ -11,6 +14,12 @@ interface CommentSectionProps {
 const commentData = TASK_DETAIL_COMMENT_MOCK_DATA;
 
 const CommentSection = ({ id, data }: CommentSectionProps) => {
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    // TODO(지권): 실제 댓글 API 호출
+    e.preventDefault();
+    // console.log("submit", id);
+  };
+
   return (
     <>
       <section className="space-y-4">
@@ -18,7 +27,8 @@ const CommentSection = ({ id, data }: CommentSectionProps) => {
           <h2>댓글</h2>
           <span className={cn("text-lg-bold text-brand-primary", "tablet:text-2lg-bold")}>{data.commentCount}</span>
         </div>
-        <form aria-label="댓글 작성" onSubmit={() => {}} className="flex items-center gap-3 w-full">
+
+        <form aria-label="댓글 작성" onSubmit={onSubmit} className="flex items-center gap-3 w-full">
           <Profile src={data.writer.image} />
           <InputReply />
         </form>
