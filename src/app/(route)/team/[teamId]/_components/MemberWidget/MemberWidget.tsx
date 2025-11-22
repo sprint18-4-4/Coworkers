6c95ca47cdc0b157d1c1d883ee598a99e27b63a4
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-22 19:32:56 +0900

    feat: 멤버 위젯 토글 형식으로 구현

--- src/app/(route)/team/[teamId]/_components/MemberWidget/MemberWidget.tsx
+++ src/app/(route)/team/[teamId]/_components/MemberWidget/MemberWidget.tsx
@@ -1,39 +1,73 @@
-import { Icon, ProfileItem } from "@/common";
+import { FloatingButton, Icon, ProfileItem } from "@/common";
+import { useState } from "react";
 
-// GET: /teamId/groups/id
-
-// TODO(상인): 사이드바 open시 없애기 -> 사이드바의 상태는 전역상태가 좋아보임
 const MemberWidget = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <aside className="absolute right-0 top-[350px] flex flex-col gap-6 w-[240px] border border-border-primary rounded-xl bg-background-primary px-5 py-[24px]">
-      <header className="flex justify-between items-center">
-        <span className="flex gap-2">
-          <span className="text-lg-medium text-text-primary">멤버</span>
-          <span className="text-lg-regular text-text-default">(4명)</span>
-        </span>
+    // TODO(상인): 일단은 맨 오른쪽 하단에 두고, 추후 변경
+    <span className="fixed right-3 bottom-3">
+      <FloatingButton iconName="user" onClick={() => setIsOpen((prev) => !prev)} />
 
-        {/* TODO(상인): 모달 머지될 시 띄우기 */}
-        <button onClick={() => {}} className="flex items-center gap-1 text-lg-semibold text-brand-primary">
-          <span>초대하기</span>
-          <Icon name="plus" className="size-4 tablet:size-4" />
-        </button>
-      </header>
+      {isOpen && (
+        <aside
+          className={
+            "absolute bottom-[65px] right-0 flex flex-col gap-6 w-[240px] border border-border-primary rounded-xl bg-background-primary px-5 py-[24px]"
+          }
+        >
+          <header className="flex justify-between items-center">
+            <span className="flex gap-2">
+              <span className="text-lg-medium text-text-primary">멤버</span>
+              <span className="text-lg-regular text-text-default">(4명)</span>
+            </span>
 
-      <ul className="flex flex-col gap-[18px]">
-        <li>
-          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
-        </li>
-        <li>
-          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
-        </li>
-        <li>
-          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
-        </li>
-        <li>
-          <ProfileItem type="memberItem" src="/profile.jpg" name="우지은" email="jieun@codeit.com" onClick={() => {}} />
-        </li>
-      </ul>
-    </aside>
+            <button onClick={() => {}} className="flex items-center gap-1 text-lg-semibold text-brand-primary">
+              <span>초대하기</span>
+              <Icon name="plus" className="size-4 tablet:size-4" />
+            </button>
+          </header>
+
+          <ul className="flex flex-col gap-[18px]">
+            <li>
+              <ProfileItem
+                type="memberItem"
+                src="/profile.jpg"
+                name="우지은"
+                email="jieun@codeit.com"
+                onClick={() => {}}
+              />
+            </li>
+            <li>
+              <ProfileItem
+                type="memberItem"
+                src="/profile.jpg"
+                name="우지은"
+                email="jieun@codeit.com"
+                onClick={() => {}}
+              />
+            </li>
+            <li>
+              <ProfileItem
+                type="memberItem"
+                src="/profile.jpg"
+                name="우지은"
+                email="jieun@codeit.com"
+                onClick={() => {}}
+              />
+            </li>
+            <li>
+              <ProfileItem
+                type="memberItem"
+                src="/profile.jpg"
+                name="우지은"
+                email="jieun@codeit.com"
+                onClick={() => {}}
+              />
+            </li>
+          </ul>
+        </aside>
+      )}
+    </span>
   );
 };
 
