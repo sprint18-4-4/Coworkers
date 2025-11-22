5724b215688cd2f2679463ce50beaad1c331bd22
wlrnjs <wlrnjs5824@gmail.com>
2025-11-23 04:17:50 +0900

    feat: 달력 날짜 선택 기능 추가

--- src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.tsx
+++ src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.tsx
@@ -1,46 +1,72 @@
 "use client";
 
 import { cn } from "@/utils";
-import { TaskListData } from "@/types";
-import { DateItem, Icon } from "@/common";
+import { DateValue, TaskListData } from "@/types";
+import { DateItem, DatePicker, Icon } from "@/common";
 import { EmptyHistory, TaskListItem } from "@/features";
 import { addDays, format } from "date-fns";
 import { useRouter } from "next/navigation";
 import { TODO_STYLES } from "../../_constants";
 import TaskPdfDownloadButton from "../TaskPdfDownloadButton/TaskPdfDownloadButton";
+import { useState } from "react";
 
 interface TodoSectionHeaderProps {
   data: TaskListData;
   selectedDate: Date;
   onClickMoveWeek: (direction: "prev" | "next") => void;
+  onClickCalendar: (date: Date) => void;
 }
 
-const TodoSectionHeader = ({ data, selectedDate, onClickMoveWeek }: TodoSectionHeaderProps) => {
+const TodoSectionHeader = ({ data, selectedDate, onClickMoveWeek, onClickCalendar }: TodoSectionHeaderProps) => {
   const monthLabel = format(selectedDate, "yyyy년 M월");
   const monthDateTime = format(selectedDate, "yyyy-MM");
+  const [isOpenCalendar, setIsOpenCalendar] = useState(false);
+
+  const handleDateSelect = (date: DateValue) => {
+    if (!date) return;
+
+    onClickCalendar(date as Date);
+    setIsOpenCalendar(false);
+  };
 
   return (
-    <header className="flex items-center justify-between">
-      <h3 className="text-2lg-bold text-text-primary">법인 등기</h3>
+    <>
+      <header className="flex items-center justify-between">
+        <h3 className="text-2lg-bold text-text-primary">법인 등기</h3>
 
-      <div className="flex items-center gap-2">
-        <time dateTime={monthDateTime} className="text-sm-medium text-text-primary">
-          {monthLabel}
-        </time>
-        <div className="flex items-center gap-1">
-          <button aria-label="이전 주" className={TODO_STYLES.buttonBase} onClick={() => onClickMoveWeek("prev")}>
-            <Icon name="leftArrow" className={TODO_STYLES.arrowBase} />
+        <div className="flex items-center gap-2">
+          <time dateTime={monthDateTime} className="text-sm-medium text-text-primary">
+            {monthLabel}
+          </time>
+          <div className="flex items-center gap-1">
+            <button aria-label="이전 주" className={TODO_STYLES.buttonBase} onClick={() => onClickMoveWeek("prev")}>
+              <Icon name="leftArrow" className={TODO_STYLES.arrowBase} />
+            </button>
+            <button
+              aria-label="다음 주"
+              className={cn(TODO_STYLES.buttonBase, "relative")}
+              onClick={() => onClickMoveWeek("next")}
+            >
+              <Icon name="rightArrow" className={TODO_STYLES.arrowBase} />
+            </button>
+          </div>
+          <button
+            aria-label="달력 열기"
+            className="size-6 rounded-full bg-background-secondary flex-center"
+            onClick={() => setIsOpenCalendar((prev) => !prev)}
+          >
+            <Icon name="calendar" className={TODO_STYLES.arrowBase} />
           </button>
-          <button aria-label="다음 주" className={TODO_STYLES.buttonBase} onClick={() => onClickMoveWeek("next")}>
-            <Icon name="rightArrow" className={TODO_STYLES.arrowBase} />
-          </button>
+          <TaskPdfDownloadButton data={data} />
         </div>
-        <button aria-label="달력 열기" className="size-6 rounded-full bg-background-secondary flex-center">
-          <Icon name="calendar" className={TODO_STYLES.arrowBase} />
-        </button>
-        <TaskPdfDownloadButton data={data} />
-      </div>
-    </header>
+      </header>
+
+      {isOpenCalendar && (
+        <div className={cn("absolute left-[10%] z-50 w-[300px]", "tablet:left-[40%]", "pc:left-[60%]")}>
+          <DatePicker value={selectedDate} onChange={handleDateSelect} />
+        </div>
+      )}
+    </>
   );
 };
 
@@ -73,21 +99,26 @@ const TodoSection = ({ data, teamId, onClickDateItem, selectedDate }: TodoSectio
         "pc:px-[42px] pc:max-w-[819px] pc:flex-1",
       )}
     >
-      <TodoSectionHeader data={data} selectedDate={selectedDate} onClickMoveWeek={handleMoveWeek} />
+      <TodoSectionHeader
+        data={data}
+        selectedDate={selectedDate}
+        onClickMoveWeek={handleMoveWeek}
+        onClickCalendar={onClickDateItem}
+      />
 
       <DateItem onClick={onClickDateItem} selectedDate={selectedDate} />
 
-      {data?.length === 0 ||
-        (!data && (
-          <EmptyHistory
-            ariaLabel="할 일이 없습니다."
-            text={
-              <span>
-                아직 완료된 작업이 없어요. <br /> 하나씩 완료해가며 히스토리를 만들어보세요!
-              </span>
-            }
-          />
-        ))}
+      {/* TODO(지권): API 연결 후 isError 변경 */}
+      {(data?.length === 0 || !data) && (
+        <EmptyHistory
+          ariaLabel="할 일이 없습니다."
+          text={
+            <span>
+              아직 완료된 작업이 없어요. <br /> 하나씩 완료해가며 히스토리를 만들어보세요!
+            </span>
+          }
+        />
+      )}
 
       <ul className="mt-[37px] flex flex-col gap-3">
         {data?.map((item) => (
