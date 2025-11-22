2433ae1439ddce6ffad7c103b84192a7f617743d
wlrnjs <wlrnjs5824@gmail.com>
2025-11-23 04:37:41 +0900

    chore: 스토리북 빌드 에러 수정

--- src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.stories.tsx
+++ src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.stories.tsx
@@ -1,15 +1,35 @@
-import { Meta, StoryObj } from "@storybook/nextjs";
+import { Meta, StoryObj } from "@storybook/react";
 import TodoSection from "./TodoSection";
+import { TASK_GROUP_MOCK_DATA } from "@/MOCK_DATA";
 
 const meta: Meta<typeof TodoSection> = {
   title: "Page/List/TodoSection",
   component: TodoSection,
   parameters: {
     layout: "centered",
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    data: { control: "object" },
+    teamId: { control: "text" },
+    selectedDate: { control: "date" },
+    onClickDateItem: { action: "dateSelected" },
+  },
+  args: {
+    teamId: "1",
+    selectedDate: new Date(),
+    onClickDateItem: () => {},
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof TodoSection>;
 
-export const TodoSectionStory: Story = {};
+export const Default: Story = {
+  args: {
+    data: TASK_GROUP_MOCK_DATA,
+  },
+};
