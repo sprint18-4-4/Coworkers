72896de7ca91ed6fa6207bb00fd09129a382f8c9
Kim_Won_Seon <gws8221@gmail.com>
2025-11-22 21:08:44 +0900

    refactor: 시멘틱 테그 수정

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -4,8 +4,8 @@ import SocialLogin from "./_components/SocialLogin/SocialLogin";
 
 const page = () => {
   return (
-    <main className={cn("w-full h-[calc(100vh-52px)] bg-background-secondary flex-center", "tablet:h-svh")}>
-      <section
+    <section className={cn("w-full h-[calc(100vh-52px)] bg-background-secondary flex-center", "tablet:h-svh")}>
+      <div
         className={cn(
           "mx-4 px-[22px] rounded-[20px]",
           "w-full min-w-[343px] min-h-[614px] max-h-[720px] max-w-[550px] bg-background-primary flex-col-center",
@@ -15,8 +15,8 @@ const page = () => {
         <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">로그인</h1>
         <LoginForm />
         <SocialLogin />
-      </section>
-    </main>
+      </div>
+    </section>
   );
 };
 
