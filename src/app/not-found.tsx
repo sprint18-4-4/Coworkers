70febfdd213fb0e25eddff90f49f290603f9b41e
wlrnjs <wlrnjs5824@gmail.com>
2025-11-23 06:39:49 +0900

    design: 이미지 사이즈 문제 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,27 @@
 import Link from "next/link";
 import { PageEmptyState } from "@/features";
+import { cn } from "@/utils";
 
 const NotFound = () => {
   return (
-    <PageEmptyState
-      title={
-        <p>
-          요청하신 페이지를 찾을 수 없습니다. <br />
-          경로가 변경되었거나 존재하지 않는 주소입니다.
-        </p>
-      }
-    >
-      <Link
-        href="/"
-        className="px-8 py-3 text-text-inverse bg-brand-primary rounded-xl hover:bg-brand-tertiary transition-colors"
+    <div className={cn("w-full h-[calc(100dvh-52px)] flex-col-center gap-[47px]", "tablet:h-dvh")}>
+      <h1 className="sr-only">404 페이지</h1>
+      <PageEmptyState
+        title={
+          <p>
+            요청하신 페이지를 찾을 수 없습니다. <br />
+            경로가 변경되었거나 존재하지 않는 주소입니다.
+          </p>
+        }
       >
-        메인 페이지 이동
-      </Link>
-    </PageEmptyState>
+        <Link
+          href="/"
+          className="px-8 py-3 text-text-inverse bg-brand-primary rounded-xl hover:bg-brand-tertiary transition-colors"
+        >
+          메인 페이지 이동
+        </Link>
+      </PageEmptyState>
+    </div>
   );
 };
 
