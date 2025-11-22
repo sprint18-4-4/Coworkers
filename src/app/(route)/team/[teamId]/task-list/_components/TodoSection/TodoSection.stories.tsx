b078bc1f46fe2097e915e0abd6050ec6757a2ca2
wlrnjs <wlrnjs5824@gmail.com>
2025-11-23 04:46:40 +0900

    chore: storybook import 경로 수정

--- src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.stories.tsx
+++ src/app/(route)/team/[teamId]/task-list/_components/TodoSection/TodoSection.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/nextjs";
 import TodoSection from "./TodoSection";
 import { TASK_GROUP_MOCK_DATA } from "@/MOCK_DATA";
 
