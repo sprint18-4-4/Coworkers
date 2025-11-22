bb86cd43aa64b694d662f2ed2264bce524ee017e
Kim_Won_Seon <gws8221@gmail.com>
2025-11-22 21:19:11 +0900

    refactor: height 속성 통일

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -4,12 +4,12 @@ import SocialLogin from "./_components/SocialLogin/SocialLogin";
 
 const page = () => {
   return (
-    <section className={cn("w-full h-[calc(100vh-52px)] bg-background-secondary flex-center", "tablet:h-svh")}>
+    <section className={cn("w-full h-[calc(100svh-52px)] bg-background-secondary flex-center", "tablet:h-svh")}>
       <div
         className={cn(
           "mx-4 px-[22px] rounded-[20px]",
           "w-full min-w-[343px] min-h-[614px] max-h-[720px] max-w-[550px] bg-background-primary flex-col-center",
-          "tablet:h-lvh tablet:mx-[61px] tablet:px-[45px]",
+          "tablet:h-svh tablet:mx-[61px] tablet:px-[45px]",
         )}
       >
         <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">로그인</h1>
