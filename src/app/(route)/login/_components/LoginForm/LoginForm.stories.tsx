3912938d4131cf9e5dee8900d8e70f4850dc4fa1
Kim_Won_Seon <gws8221@gmail.com>
2025-11-24 04:24:33 +0900

    feat: 스토리북 추가

---
+++ src/app/(route)/login/_components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import LoginForm from "./LoginForm";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: false },
+    mutations: { retry: false },
+  },
+});
+
+const meta: Meta<typeof LoginForm> = {
+  title: "Page/Login/LoginForm",
+  component: LoginForm,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <div className="w-[400px] border p-6 rounded-2xl bg-white shadow-sm">
+          <Story />
+        </div>
+        <Toaster position="bottom-center" />
+      </QueryClientProvider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof LoginForm>;
+
+export const Default: Story = {};
---
+++ src/app/(route)/login/_components/SocialLogin/SociaLogin.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import SocialLogin from "./SocialLogin";
+
+const meta: Meta<typeof SocialLogin> = {
+  title: "Page/Login/SocialLogin",
+  component: SocialLogin,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SocialLogin>;
+
+export const Default: Story = {
+  render: () => (
+    <div>
+      <SocialLogin />
+    </div>
+  ),
+};
