4aafa6530b14168582da4eb613299930d5dbfcd1
Kim_Won_Seon <gws8221@gmail.com>
2025-11-24 02:25:20 +0900

    feat: 비밀번호 재설정 모달 연결

--- src/app/(route)/login/_components/index.ts
+++ src/app/(route)/login/_components/index.ts
@@ -1,2 +1,3 @@
 export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as SocialLogin } from "./SocialLogin/SocialLogin";
+export { default as ResetPassword } from "./ResetPassword/ResetPassword";
