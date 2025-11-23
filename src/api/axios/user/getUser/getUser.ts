c6c59740960a30b1ac1ccf9ec5727fd4fb096ece
Kim_Won_Seon <gws8221@gmail.com>
2025-11-23 16:13:53 +0900

    feat: getUser API 연동

---
+++ src/api/axios/user/getUser/getUser.ts
@@ -0,0 +1,7 @@
+import { instance } from "@/lib";
+import { UserResponse } from "@/types";
+
+export const getUser = async (): Promise<UserResponse> => {
+  const { data } = await instance.get<UserResponse>("/user");
+  return data;
+};
