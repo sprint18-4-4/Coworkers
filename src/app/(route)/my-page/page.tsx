342ba4412196310fd31f0491b3b8c1e5b8f58df9
Kim_Won_Seon <gws8221@gmail.com>
2025-11-25 03:24:37 +0900

    refactor: 테일윈드 클래스 정리(가독성)

--- src/app/(route)/my-page/page.tsx
+++ src/app/(route)/my-page/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { cn } from "@/utils";
-import { MyProfileForm, SecessionButton } from "./_components/";
+import { MyProfileForm, SecessionButton } from "./_components";
 // import { SaveChangesSnackbar } from "@/common";
 
 const MyPage = () => {
   return (
     <section className={cn("w-full h-[calc(100svh-52px)] flex-center", "tablet:h-svh")}>
-      <div className="w-full h-full mx-4 tablet:mx-[60px] min-w-[343px] max-w-[940px] max-h-[600px] tablet:max-h-[720px] flex-col-center gap-7">
+      <div
+        className={cn(
+          "w-full h-full mx-4 tablet:mx-[60px] flex-col-center gap-7",
+          "min-w-[343px] max-w-[940px] max-h-[600px] tablet:max-h-[720px]",
+        )}
+      >
         <div
           className={cn(
-            "w-full h-full px-5 bg-background-primary rounded-[20px]  flex-col-center gap-[35px]",
-            "tablet:px-[45px]",
+            "w-full h-full bg-background-primary rounded-[20px]  flex-col-center gap-[35px]",
+            "px-5 tablet:px-[45px]",
           )}
         >
           <h2 className="w-full text-left text-xl-bold text-text-primary tablet:text-2xl-bold">계정 설정</h2>
