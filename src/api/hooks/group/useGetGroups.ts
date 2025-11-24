420deac39f73c8524e4e8103d6d17266037b33b5
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-25 06:46:04 +0900

    feat: useGetGroups (useQuery) 구현

---
+++ src/api/hooks/group/useGetGroups.ts
@@ -0,0 +1,16 @@
+import { getGroups } from "@/api/axios";
+import { GetGroupsRequest } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const useGetGroups = ({ id }: GetGroupsRequest) => {
+  return useQuery({
+    queryKey: ["groups", id],
+    queryFn: () => getGroups({ id }),
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 60 * 24,
+    retry: 1,
+    enabled: !!id,
+  });
+};
+
+export default useGetGroups;
--- src/api/hooks/index.ts
+++ src/api/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as usePostLogin } from "./auth/login/usePostLogin";
 export { default as useGetUser } from "./user/useGetUser";
+export { default as useGetGroups } from "./group/useGetGroups";
