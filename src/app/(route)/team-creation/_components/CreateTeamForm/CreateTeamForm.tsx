4f90959feddb167b25fca869c4f5753e1e26afc9
Kim_Won_Seon <gws8221@gmail.com>
2025-11-24 19:49:45 +0900

    feat: react-responsive 라이브러리 설치 및 적용

--- src/app/(route)/team-creation/_components/CreateTeamForm/CreateTeamForm.tsx
+++ src/app/(route)/team-creation/_components/CreateTeamForm/CreateTeamForm.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 import { ProfileEdit, Input, BaseButton } from "@/common";
 
 const CreateTeamForm = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const mobile = useMediaQuery({ query: "(max-width: 430px)" });
+
+  useEffect(() => {
+    setIsMobile(mobile);
+  }, [mobile]);
+
   return (
     <form className="w-full flex-col-center gap-10">
       <div className="w-full flex-col-center gap-3 tablet:gap-6">
-        <ProfileEdit src={null} onChange={() => {}} size="lg" />
+        <ProfileEdit src={null} onChange={() => {}} size={isMobile ? "md" : "lg"} />
         <Input label="팀 이름" type="text" placeholder="팀 이름을 입력해주세요." />
       </div>
       <div className="w-full flex-col-center gap-5">
