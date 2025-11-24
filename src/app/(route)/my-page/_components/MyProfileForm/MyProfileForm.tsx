1dc4d60cd42c629be898de17bb24d66e8b9c9e22
Kim_Won_Seon <gws8221@gmail.com>
2025-11-25 02:52:29 +0900

    refactor: useDevice 훅 적용

--- src/app/(route)/my-page/_components/MyProfileForm/MyProfileForm.tsx
+++ src/app/(route)/my-page/_components/MyProfileForm/MyProfileForm.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { ProfileEdit, Input } from "@/common";
+import { useDevice } from "@/hooks";
 import { PwInputSection } from "..";
 
 const MyProfileForm = () => {
+  const device = useDevice();
+
+  const profileSize = device.isMobile ? "md" : "lg";
+
   return (
     <form id="profileForm" className="w-full flex-col-center gap-6">
-      <ProfileEdit src={null} onChange={() => {}} size="lg" />
+      <ProfileEdit src={null} onChange={() => {}} size={profileSize} />
       <Input label="이름" type="text" defaultValue="아무개" placeholder="이름을 입력해주세요." />
       <Input label="이메일" type="email" defaultValue={"text@example.com"} disabled />
       <PwInputSection />
