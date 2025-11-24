c2794e27323e144dea38401b65689b2acfd4abcc
wlrnjs <wlrnjs5824@gmail.com>
2025-11-25 03:16:19 +0900

    rename: import 수정

--- src/app/(route)/my-history/_components/WorkHistorySection/WorkHistorySection.tsx
+++ src/app/(route)/my-history/_components/WorkHistorySection/WorkHistorySection.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/utils";
-import { Chip } from "@/common";
+import { TaskListItemType } from "@/types";
 import { EmptyState, TaskListItem } from "@/features";
-import { TaskListItemType } from "@/types/TaskListItemType";
 
 const WorkHistorySection = ({ data }: { data: TaskListItemType[] }) => {
   return (
@@ -14,21 +13,13 @@ const WorkHistorySection = ({ data }: { data: TaskListItemType[] }) => {
       {/* TODO(지권): 에러, 로딩 상태 처리 필요 */}
       {data?.length === 0 && <EmptyState ariaLabel="업무 기록 없음" text="완료한 업무 기록이 없습니다." />}
       {data?.length > 0 && (
-        <>
-          <nav aria-label="업무 카테고리" className="flex items-start gap-1 text-nowrap overflow-x-auto pc:hidden">
-            {Array.from({ length: 3 }, (_, i) => (
-              <Chip key={i} title="법인 등기" count={3} />
+        <section>
+          <ul className="flex flex-col gap-3">
+            {data.map((item) => (
+              <TaskListItem key={item.id} item={item} />
             ))}
-          </nav>
-
-          <section>
-            <ul className="flex flex-col gap-3">
-              {data.map((item) => (
-                <TaskListItem key={item.id} item={item} />
-              ))}
-            </ul>
-          </section>
-        </>
+          </ul>
+        </section>
       )}
     </article>
   );
