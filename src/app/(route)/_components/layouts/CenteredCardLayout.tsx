019c6a898b34bb801a48dadbfbfde51a64263c04
Kim_Won_Seon <gws8221@gmail.com>
2025-11-24 14:15:08 +0900

    design: 카드 레이아웃 추가

---
+++ src/app/(route)/_components/layouts/CenteredCardLayout.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/utils";
+import { ReactNode } from "react";
+
+/**
+ * @author KimWonSeon
+ * @description 로그인, 회원가입, 팀 생성하기 등 여러 페이지에서 사용될 가운데 카드 레이아웃 디자인 컴포넌트입니다.
+ *
+ * @param children - 레이아웃 내부에 렌더링될 메인 콘텐츠입니다
+ * @param className - 카드 부분을 담당하는 className 입니다. 페이지마다 크기가 달라 width와 height를 조정해주셔야합니다.
+ *
+ * @example
+ * ```tsx
+ * <CenteredCardLayout className="min-w-[343px] min-h-[692px] max-h-[840px] max-w-[550px]">
+ *  <h1>로그인</h1>
+ *  <SignUpForm />
+ *  <SocialSingUp />
+ * </CenteredCardLayout>
+ */
+
+interface CenteredCardLayoutProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const CenteredCardLayout = ({ children, className }: CenteredCardLayoutProps) => {
+  return (
+    <section className={cn("w-full h-[calc(100svh-52px)] flex-center", "tablet:h-svh")}>
+      <div
+        className={cn(
+          "mx-4 px-[22px] rounded-[20px]",
+          "w-full bg-background-primary flex-col-center",
+          "tablet:h-svh tablet:mx-[61px] tablet:px-[45px]",
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default CenteredCardLayout;
--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,22 +1,13 @@
-import { cn } from "@/utils";
-import LoginForm from "./_components/LoginForm/LoginForm";
-import SocialLogin from "./_components/SocialLogin/SocialLogin";
+import { LoginForm, SocialLogin } from "./_components";
+import { CenteredCardLayout } from "../_components";
 
 const LoginPage = () => {
   return (
-    <section className={cn("w-full h-[calc(100svh-52px)] bg-background-secondary flex-center", "tablet:h-svh")}>
-      <div
-        className={cn(
-          "mx-4 px-[22px] rounded-[20px]",
-          "w-full min-w-[343px] min-h-[614px] max-w-[550px] bg-background-primary flex-col-center",
-          "tablet:mx-[61px] tablet:px-[45px] tablet:py-[70px]",
-        )}
-      >
-        <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">로그인</h1>
-        <LoginForm />
-        <SocialLogin />
-      </div>
-    </section>
+    <CenteredCardLayout className="min-w-[343px] min-h-[614px] max-h-[720px] max-w-[550px]">
+      <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">로그인</h1>
+      <LoginForm />
+      <SocialLogin />
+    </CenteredCardLayout>
   );
 };
 
