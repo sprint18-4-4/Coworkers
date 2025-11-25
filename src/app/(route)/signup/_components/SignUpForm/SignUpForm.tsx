6aaa694ceb5907205fb17dbc3357388db19813c0
Kim_Won_Seon <gws8221@gmail.com>
2025-11-25 16:05:04 +0900

    refactor: 로그인 이동 링크 스타일 속성 수정

--- src/app/(route)/signup/_components/SignUpForm/SignUpForm.tsx
+++ src/app/(route)/signup/_components/SignUpForm/SignUpForm.tsx
@@ -50,9 +50,11 @@ const SignUpForm = () => {
           error={errors.passwordConfirm}
         />
       </div>
-      <Link href="/login" className="mt-3 text-brand-primary text-md-medium tablet:text-lg-medium text-right">
-        이미 계정이 있으신가요?
-      </Link>
+      <div className="flex justify-end">
+        <Link href="/login" className="mt-3 text-brand-primary text-md-medium tablet:text-lg-medium">
+          이미 계정이 있으신가요?
+        </Link>
+      </div>
       <div className="text-lg-semibold mt-10">
         <BaseButton variant="solid" size="large" type="submit" disabled={meta.isLoading || !meta.isValid}>
           회원가입
