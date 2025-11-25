1e9aa48985db42f63f92b7961f689c3d73e5d90f
Kim_Won_Seon <gws8221@gmail.com>
2025-11-26 03:12:36 +0900

    feat: page.tsx  스피너 추가

--- src/app/(route)/login/kakao/page.tsx
+++ src/app/(route)/login/kakao/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import { useKakaoAuth } from "../../_components/SocialAuthSection/_hooks";
 
-const KakaoRedirectPage = () => {
+// TODO(김원선): 로딩 스피너 구현시 변경
+const LoginContent = () => {
   const { isLoading } = useKakaoAuth();
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-4 bg-background-primary">
-      {/* 로딩 스피너 구현시 변경 */}
       {isLoading ? (
         <>
           <div className="animate-spin size-10 border-4 border-blue-500 border-t-transparent rounded-full" />
@@ -17,6 +18,14 @@ const KakaoRedirectPage = () => {
         <p className="text-text-weak">잠시만 기다려주세요...</p>
       )}
     </div>
+  );
+};
+
+const KakaoRedirectPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginContent />
+    </Suspense>
   );
 };
 
