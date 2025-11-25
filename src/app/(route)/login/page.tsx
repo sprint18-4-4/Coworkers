901e30f272e2f673b29e0adb8080cbf063de28c3
Kim_Won_Seon <gws8221@gmail.com>
2025-11-25 23:03:12 +0900

    feat: CenteredCardLayout에 공통 타이틀(h2) 추가

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -3,8 +3,7 @@ import { CenteredCardLayout } from "../_components";
 
 const page = () => {
   return (
-    <CenteredCardLayout className="min-w-[343px] min-h-[614px] max-h-[720px] max-w-[550px]">
-      <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">로그인</h1>
+    <CenteredCardLayout className="min-w-[343px] min-h-[614px] max-h-[720px] max-w-[550px]" title="로그인">
       <LoginForm />
       <SocialLogin />
     </CenteredCardLayout>
