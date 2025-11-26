dd7d7daba33a71be761896d56da28ce007efd471
wlrnjs <wlrnjs5824@gmail.com>
2025-11-27 05:35:19 +0900

    fix: 스토리북 수정

--- src/app/(route)/team/[teamId]/task-list/[taskListId]/_detail/_components/_internal/HeaderSection/HeaderSection.stories.tsx
+++ src/app/(route)/team/[teamId]/task-list/[taskListId]/_detail/_components/_internal/HeaderSection/HeaderSection.stories.tsx
@@ -1,6 +1,9 @@
 import { Meta, StoryObj } from "@storybook/nextjs";
 import HeaderSection from "./HeaderSection";
 import { TASK_DETAIL_MOCK_DATA } from "@/MOCK_DATA";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const meta: Meta<typeof HeaderSection> = {
   title: "Page/ListDetail/HeaderSection",
@@ -11,9 +14,11 @@ const meta: Meta<typeof HeaderSection> = {
   },
   decorators: [
     (Story) => (
-      <div className="w-[600px]">
-        <Story />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="w-[600px]">
+          <Story />
+        </div>
+      </QueryClientProvider>
     ),
   ],
 };
@@ -21,8 +26,24 @@ const meta: Meta<typeof HeaderSection> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const MOCK_PATH = {
+  id: "27453",
+  teamId: "3449",
+  taskListId: "4871",
+};
+
 export const Default: Story = {
   args: {
     data: TASK_DETAIL_MOCK_DATA,
+    isDone: false,
+    taskPath: MOCK_PATH,
+  },
+};
+
+export const Done: Story = {
+  args: {
+    data: TASK_DETAIL_MOCK_DATA,
+    isDone: true,
+    taskPath: MOCK_PATH,
   },
 };
