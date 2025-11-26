9820d3651621830dfa4ccf0284a719f4cd8430f2
Kim_Won_Seon <gws8221@gmail.com>
2025-11-27 05:34:25 +0900

    feat: 비밀번호 재설정 페이지 구현

--- src/app/(route)/reset-password/page.tsx
+++ src/app/(route)/reset-password/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { cn } from "@/utils";
 import NewPasswordForm from "./_components/NewPasswordForm/NewPasswordForm";
 
@@ -13,7 +14,15 @@ const ResetPasswordPage = () => {
         )}
       >
         <h1 className="text-xl-bold text-text-primary tablet:text-2xl-bold">비밀번호 재설정</h1>
-        <NewPasswordForm />
+        <Suspense
+          fallback={
+            <div className="w-full flex-center">
+              <span className="text-md-medium text-text-disabled">로딩 중...</span>
+            </div>
+          }
+        >
+          <NewPasswordForm />
+        </Suspense>
       </div>
     </section>
   );
