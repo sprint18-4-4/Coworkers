5c3a3a9e13ac1abcd2d51730c2fce58da11953f7
wlrnjs <wlrnjs5824@gmail.com>
2025-11-27 09:30:15 +0900

    refactor: POST API 수정

--- src/app/(route)/team/[teamId]/task-list/[taskListId]/_components/TaskListCreateModal/TaskListCreateModal.tsx
+++ src/app/(route)/team/[teamId]/task-list/[taskListId]/_components/TaskListCreateModal/TaskListCreateModal.tsx
@@ -10,13 +10,13 @@ interface TaskListCreateModalProps {
 
 const TaskListCreateModal = ({ isOpen, onClose, groupId }: TaskListCreateModalProps) => {
   const [todoName, setTodoName] = useState("");
-  const { mutate, isPending } = usePostTodo({ groupId, name: todoName });
+  const { mutate, isPending } = usePostTodo();
 
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleCreateTodo = (e: FormEvent<HTMLFormElement>) => {
     if (!todoName || todoName.trim() === "") return;
 
     e.preventDefault();
-    mutate();
+    mutate({ groupId, name: todoName });
     setTodoName("");
     onClose();
   };
@@ -25,7 +25,7 @@ const TaskListCreateModal = ({ isOpen, onClose, groupId }: TaskListCreateModalPr
     <Modal isOpen={isOpen} onClose={onClose} className="flex-col-center gap-4 px-4 py-8">
       <Modal.CloseIcon onClose={onClose} />
       <h2 className="text-lg-medium text-text-primary">할 일 목록</h2>
-      <form onSubmit={onSubmit} className="flex-col-center gap-4 w-[280px]">
+      <form onSubmit={handleCreateTodo} className="flex-col-center gap-4 w-[280px]">
         <Input
           autoFocus
           placeholder="목록 명을 입력해주세요."
