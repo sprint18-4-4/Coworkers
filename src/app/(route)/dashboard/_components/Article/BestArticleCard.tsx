54c6326bc4349c8657912201a9f1425776e6d37d
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-27 21:24:22 +0900

    feat: article detail page 이동 구현

--- src/app/(route)/dashboard/_components/Article/BestArticleCard.tsx
+++ src/app/(route)/dashboard/_components/Article/BestArticleCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import ArticleWriter from "./_internal/ArticleWriter";
 import ArticleLike from "./_internal/ArticleLike";
 import { Icon } from "@/common";
+import Link from "next/link";
 
 const BestArticleCard = ({ articleId }: { articleId: number }) => {
   const { data: article } = useGetArticle({ articleId });
@@ -15,25 +16,27 @@ const BestArticleCard = ({ articleId }: { articleId: number }) => {
   }
 
   return (
-    <article className="flex flex-col gap-3 max-w-[350px] pc:gap-4 rounded-[20px] border bg-background-primary px-5 py-6">
-      <ArticleBestBadge />
-      <div className="flex justify-between gap-4">
-        <div className="flex flex-col gap-3">
-          <ArticleTitle title={article?.title} />
-          <ArticleContent content={article?.content} />
+    <Link href={`/dashboard/${articleId}`} className="block">
+      <article className="flex flex-col gap-3 max-w-[350px] pc:gap-4 rounded-[20px] border bg-background-primary px-5 py-6">
+        <ArticleBestBadge />
+        <div className="flex justify-between gap-4">
+          <div className="flex flex-col gap-3">
+            <ArticleTitle title={article?.title} />
+            <ArticleContent content={article?.content} />
+          </div>
+          {/* 정상적이지 않은 URL, 혹은 null 값으로 왔을 때 왔을 때 에러처리 */}
+          {/* {article.image && <Image src={article.image} alt="게시글 이미지" width={60} height={60} />} */}
         </div>
-        {/* 정상적이지 않은 URL, 혹은 null 값으로 왔을 때 왔을 때 에러처리 */}
-        {/* {article.image && <Image src={article.image} alt="게시글 이미지" width={60} height={60} />} */}
-      </div>
 
-      <footer className="flex justify-between items-center">
-        <ArticleWriter nickname={article.writer.nickname} createdAt={article.createdAt} />
-        <span className="flex items-center gap-1">
-          <Icon name="heartDefault" className="size-4 tablet:size-4" />
-          <ArticleLike likeCount={article.likeCount} />
-        </span>
-      </footer>
-    </article>
+        <footer className="flex justify-between items-center">
+          <ArticleWriter nickname={article.writer.nickname} createdAt={article.createdAt} />
+          <span className="flex items-center gap-1">
+            <Icon name="heartDefault" className="size-4 tablet:size-4" />
+            <ArticleLike likeCount={article.likeCount} />
+          </span>
+        </footer>
+      </article>
+    </Link>
   );
 };
 
--- src/app/(route)/dashboard/_components/Article/DefaultArticleCard.tsx
+++ src/app/(route)/dashboard/_components/Article/DefaultArticleCard.tsx
@@ -4,6 +4,7 @@ import ArticleContent from "./_internal/ArticleContent";
 import ArticleWriter from "./_internal/ArticleWriter";
 import ArticleLike from "./_internal/ArticleLike";
 import { Icon } from "@/common";
+import Link from "next/link";
 
 const DefaultArticleCard = ({ articleId }: { articleId: number }) => {
   const { data: article } = useGetArticle({ articleId });
@@ -13,23 +14,25 @@ const DefaultArticleCard = ({ articleId }: { articleId: number }) => {
   }
 
   return (
-    <article className="flex flex-col gap-3 w-full pc:gap-4 rounded-[20px] border bg-background-primary px-5 py-6">
-      <div className="flex justify-between gap-4">
-        <div className="flex flex-col gap-3">
-          <ArticleTitle title={article?.title} />
-          <ArticleContent content={article?.content} />
+    <Link href={`/dashboard/${article.id}`} className="block">
+      <article className="flex flex-col gap-3 w-full pc:gap-4 rounded-[20px] border bg-background-primary px-5 py-6">
+        <div className="flex justify-between gap-4">
+          <div className="flex flex-col gap-3">
+            <ArticleTitle title={article?.title} />
+            <ArticleContent content={article?.content} />
+          </div>
+          {/* 정상적이지 않은 URL, 혹은 null 값으로 왔을 때 왔을 때 에러처리 */}
+          {/* {article.image && <Image src={article.image} alt="게시글 이미지" width={60} height={60} />} */}
         </div>
-        {/* 정상적이지 않은 URL, 혹은 null 값으로 왔을 때 왔을 때 에러처리 */}
-        {/* {article.image && <Image src={article.image} alt="게시글 이미지" width={60} height={60} />} */}
-      </div>
-      <footer className="flex justify-between items-center">
-        <ArticleWriter nickname={article.writer.nickname} createdAt={article.createdAt} />
-        <span className="flex items-center gap-1">
-          <Icon name="heartDefault" className="size-4 tablet:size-4" />
-          <ArticleLike likeCount={article.likeCount} />
-        </span>
-      </footer>
-    </article>
+        <footer className="flex justify-between items-center">
+          <ArticleWriter nickname={article.writer.nickname} createdAt={article.createdAt} />
+          <span className="flex items-center gap-1">
+            <Icon name="heartDefault" className="size-4 tablet:size-4" />
+            <ArticleLike likeCount={article.likeCount} />
+          </span>
+        </footer>
+      </article>
+    </Link>
   );
 };
 
