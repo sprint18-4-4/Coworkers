38eda78d28924ea0f766b486542568aecba4c47b
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-27 09:06:14 +0900

    feat: 자유게시판 레이아웃 구현

--- src/app/(route)/dashboard/_components/DashBoardBestPosts/page.tsx
+++ src/app/(route)/dashboard/_components/DashBoardBestPosts/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const DashBoardBestPosts = () => {
-  return <div>page</div>;
+  return (
+    <section className="mt-10 bg-background-secondary rounded-[20px]">
+      <h3 className="text-text-primary text-xl-bold">베스트 게시글</h3>
+    </section>
+  );
 };
 
 export default DashBoardBestPosts;
