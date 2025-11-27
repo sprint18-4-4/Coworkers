a2448ed5526ec4de503f32719be31571cf5d8c7e
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-27 11:22:49 +0900

    fix: 카드 스타일 수정

--- src/app/(route)/dashboard/_components/Article/BestArticleCard.tsx
+++ src/app/(route)/dashboard/_components/Article/BestArticleCard.tsx
@@ -2,7 +2,7 @@ import ArticleBestBadge from "./_internal/ArticleBestBadge";
 
 const BestArticleCard = () => {
   return (
-    <article>
+    <article className="flex flex-col gap-4 max-w-[350px] h-[206px] rounded-[20px] border bg-background-primary px-5 py-6">
       <ArticleBestBadge />
     </article>
   );
--- src/app/(route)/dashboard/_components/Section/DashBoardBestArticles/DashBoardBestArticles.tsx
+++ src/app/(route)/dashboard/_components/Section/DashBoardBestArticles/DashBoardBestArticles.tsx
@@ -2,8 +2,8 @@ import BestArticleCard from "../../Article/BestArticleCard";
 
 const DashBoardBestArticles = () => {
   return (
-    <section className="mt-10 bg-background-secondary rounded-[20px]">
-      <h3 className="text-text-primary text-xl-bold">베스트 게시글</h3>
+    <section className="py-10 px-8 mt-10 bg-background-secondary rounded-[20px]">
+      <h3 className="text-text-primary text-xl-bold mb-6">베스트 게시글</h3>
       <BestArticleCard />
     </section>
   );
