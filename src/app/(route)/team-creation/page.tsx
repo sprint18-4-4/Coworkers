9fbff0649588ff4b6efe7b3c9b779339202f9d94
Kim_Won_Seon <gws8221@gmail.com>
2025-11-29 02:05:04 +0900

    refactor: 스타일 수정

--- src/app/(route)/team-creation/page.tsx
+++ src/app/(route)/team-creation/page.tsx
@@ -4,7 +4,7 @@ import { TeamCreateForm } from "./_components/";
 const TeamCreatePage = () => {
   return (
     <CenteredCardLayout
-      className="justify-between w-full min-w-[343px] max-w-[550px] h-[464px] tablet:h-[543px] gap-8 pt-[52.5px] pb-[72.5px] tablet:py-[62px]"
+      className="w-full min-w-[343px] max-w-[550px] h-[464px] justify-between tablet:h-[543px] gap-8 pt-[52.5px] pb-[72.5px] tablet:py-[62px]"
       title="팀 생성하기"
       titleClassName="w-full text-left text-xl-bold"
     >
