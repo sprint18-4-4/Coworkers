aa08d498eef4e3356c9fa2b8445bf5cde79cde52
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-28 22:06:12 +0900

    feat: article detail페이지'

--- src/app/(route)/dashboard/[id]/_components/ArticleDetail.tsx
+++ src/app/(route)/dashboard/[id]/_components/ArticleDetail.tsx
@@ -2,17 +2,20 @@
 
 import { useGetArticle } from "@/api/hooks";
 import { useParams, useRouter } from "next/navigation";
-import { Dropdown, Icon } from "@/common";
 import ArticleTitle from "../../_components/Article/_internal/ArticleTitle";
 import { useDevice } from "@/hooks";
+import { Dropdown, InputReply, Profile } from "@/common";
 import ArticleWriter from "../../_components/Article/_internal/ArticleWriter";
 import ArticleContent from "../../_components/Article/_internal/ArticleContent";
+import { useState } from "react";
 
 const ArticleDetail = () => {
   const router = useRouter();
   const { id } = useParams();
   const { isPc } = useDevice();
   const { data: article } = useGetArticle({ articleId: Number(id) });
+
+  const [commentValue, setCommentValue] = useState("");
 
   const options = [
     { label: "수정하기", action: () => router.push(`/dashboard/${id}/edit`) },
@@ -23,6 +26,7 @@ const ArticleDetail = () => {
     return null;
   }
 
+  console.log(article);
   return (
     <section className="bg-background-primary max-w-[900px] rounded-[20px] py-14 px-10">
       <header className="flex flex-col gap-4 border-b border-border-primary">
@@ -35,9 +39,22 @@ const ArticleDetail = () => {
         </div>
       </header>
 
-      <div>
+      <div className="pt-6">
         <ArticleContent content={article.content} />
       </div>
+
+      <footer className="py-10">
+        <section>
+          <div className="flex items-center gap-1 text-lg-bold text-text-primary">
+            <span>댓글</span>
+            <span className="text-brand-primary">{article.commentCount}</span>
+          </div>
+          <form aria-label="댓글 작성">
+            {/* <Profile src={} /> */}
+            <InputReply value={commentValue} onChange={setCommentValue} isSubmitting={false} />
+          </form>
+        </section>
+      </footer>
     </section>
   );
 };
