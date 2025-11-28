5b8dab8659513addd68b279a71649423cec6a4ba
Kim_Won_Seon <gws8221@gmail.com>
2025-11-29 01:02:05 +0900

    refactor: interface로 변경

--- src/api/axios/auth/types.ts
+++ src/api/axios/auth/types.ts
@@ -1,46 +1,46 @@
 import { AuthToken, User } from "@/types";
 
 // 로그인
-export type LoginRequest = {
+export interface LoginRequest {
   email: string;
   password: string;
-};
+}
 
-export type LoginResponse = AuthToken & {
+export interface LoginResponse extends AuthToken {
   user: User;
-};
+}
 
 // 회원가입
-export type SignUpRequest = {
+export interface SignUpRequest {
   email: string;
   nickname: string;
   password: string;
   passwordConfirmation: string;
-};
+}
 
-export type SignUpResponse = AuthToken & {
+export interface SignUpResponse extends AuthToken {
   user: User;
-};
+}
 
 // 비밀번호 재설정 URL 이메일 보내기
 
-export type ResetPasswordRequest = {
+export interface ResetPasswordRequest {
   email: string;
   redirectUrl: string;
-};
+}
 
-export type ResetPasswordResponse = {
+export interface ResetPasswordResponse {
   message: string;
-};
+}
 
 // 비밀번호 재설정
 
-export type PatchResetPasswordRequest = {
+export interface PatchResetPasswordRequest {
   password: string;
   passwordConfirmation: string;
   token: string;
-};
+}
 
-export type PatchResetPasswordResponse = {
+export interface PatchResetPasswordResponse {
   message: string;
-};
+}
