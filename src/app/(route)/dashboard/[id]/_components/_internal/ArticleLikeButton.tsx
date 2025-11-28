d686fa11002e5fe64177e6d3c8cf9e8af5108384
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-11-29 04:06:16 +0900

    feat: 좋아요 기능 완성'

--- src/app/(route)/dashboard/[id]/_components/_internal/ArticleLikeButton.tsx
+++ src/app/(route)/dashboard/[id]/_components/_internal/ArticleLikeButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useGetArticle } from "@/api/hooks";
+import { useDeleteArticleLike, useGetArticle, usePostArticleLike } from "@/api/hooks";
 import { FloatingButton, Icon } from "@/common";
 import { useParams } from "next/navigation";
 import ArticleLike from "../../../_components/Article/_internal/ArticleLike";
@@ -9,20 +9,38 @@ import { useDevice } from "@/hooks";
 const ArticleLikeButton = () => {
   const { id } = useParams();
   const { isPc } = useDevice();
-  const { data: article } = useGetArticle({ articleId: Number(id) });
+
+  const articleId = Number(id);
+
+  const { data: article } = useGetArticle({ articleId });
+  const { mutate: postArticleLike } = usePostArticleLike();
+  const { mutate: deleteArticleLike } = useDeleteArticleLike();
 
   if (!article) {
     return null;
   }
+  const { isLiked } = article;
+
+  const handleLikeClick = () => {
+    if (isLiked) {
+      deleteArticleLike({ articleId });
+    } else {
+      postArticleLike({ articleId });
+    }
+  };
 
   return isPc ? (
     <span className="flex-col-center gap-2 absolute right-[-120px]">
-      <FloatingButton iconName="heartDefault" className="bg-background-inverse size-[64px]" />
+      <FloatingButton
+        iconName={isLiked ? "heartActive" : "heartDefault"}
+        className="bg-background-inverse size-[64px]"
+        onClick={handleLikeClick}
+      />
       <ArticleLike likeCount={article?.likeCount} />
     </span>
   ) : (
-    <span className="flex items-center gap-1 absolute right-0">
-      <Icon name="heartDefault" className="size-6 tablet:size-6" />
+    <span className="flex items-center gap-1 absolute right-0 cursor-pointer" onClick={handleLikeClick}>
+      <Icon name={isLiked ? "heartActive" : "heartDefault"} className="size-6 tablet:size-6" />
       <ArticleLike likeCount={article?.likeCount} />
     </span>
   );
