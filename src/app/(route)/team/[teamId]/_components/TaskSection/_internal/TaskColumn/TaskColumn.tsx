5d9d24919c3ed8a00d8ea9f16785c3f2511a43a3
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-12-01 08:18:29 +0900

    feat: 할 일 생성 구현

--- src/app/(route)/team/[teamId]/_components/TaskSection/_internal/TaskColumn/TaskColumn.tsx
+++ src/app/(route)/team/[teamId]/_components/TaskSection/_internal/TaskColumn/TaskColumn.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { BaseButton, Input, Modal, ProgressButton } from "@/common";
 import TaskCard from "../TaskCard/TaskCard";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import { usePostTaskList } from "@/api/hooks";
+import { useParams } from "next/navigation";
 
 type ColumnTitle = "할 일" | "진행중" | "완료";
 
@@ -10,6 +14,10 @@ interface TaskColumnProps {
 }
 
 const TaskColumn = ({ title, items }: TaskColumnProps) => {
+  const { mutate: postTaskList, isPending } = usePostTaskList();
+  const { teamId } = useParams();
+
+  const [taskValue, setTaskValue] = useState("");
   const [isOpenCreateTaskModal, setIsOpenCreateTaskModal] = useState(false);
 
   const isCompletedColumn = () => {
@@ -20,7 +28,17 @@ const TaskColumn = ({ title, items }: TaskColumnProps) => {
     return false;
   };
 
-  const handleCreateTaskClick = () => {};
+  const handleCreateTaskClick = () => {
+    postTaskList(
+      { groupId: Number(teamId), name: taskValue },
+      {
+        onSuccess: () => {
+          setIsOpenCreateTaskModal(false);
+          setTaskValue("");
+        },
+      },
+    );
+  };
 
   return (
     <section className="flex-1 flex flex-col gap-5">
@@ -33,11 +51,15 @@ const TaskColumn = ({ title, items }: TaskColumnProps) => {
         <Modal.CloseIcon onClose={() => setIsOpenCreateTaskModal(false)} />
         <Modal.Body className="flex-col-center gap-2">
           <h3>할 일 목록</h3>
-          <Input placeholder="목록 명을 입력해주세요." />
+          <Input
+            value={taskValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setTaskValue(e.target.value)}
+            placeholder="목록 명을 입력해주세요."
+          />
         </Modal.Body>
         <Modal.Footer>
-          <BaseButton variant="solid" size="large" onClick={handleCreateTaskClick}>
-            만들기
+          <BaseButton variant="solid" size="large" onClick={handleCreateTaskClick} disabled={isPending}>
+            {isPending ? "만드는 중..." : "만들기"}
           </BaseButton>
         </Modal.Footer>
       </Modal>
