71fe8920cbfae634bbf60f073be10cbceb42ec08
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-12-02 07:22:27 +0900

    refactor: 글쓰기 후 목록으로 돌아왔을 때 최신화 안되는 버그 수정

--- src/api/hooks/article/usePostArticle.ts
+++ src/api/hooks/article/usePostArticle.ts
@@ -1,15 +1,17 @@
 import { postArticle } from "@/api/axios";
 import { toastKit } from "@/utils";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 const usePostArticle = () => {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const { success, error } = toastKit();
   return useMutation({
     mutationFn: postArticle,
     onSuccess: (data) => {
       success("게시물 등록을 성공했습니다.");
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
       router.replace(`/dashboard/${data.id}`);
     },
     onError: () => {
