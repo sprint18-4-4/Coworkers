7a8645f838df281ebeee35c1073179f03027054c
wlrnjs <wlrnjs5824@gmail.com>
2025-12-02 03:07:43 +0900

    rename: MOCK_DATA 파일 제거

--- src/app/(route)/team/[teamId]/task-list/[taskListId]/_detail/_components/_internal/HeaderSection/HeaderSection.stories.tsx
+++ src/app/(route)/team/[teamId]/task-list/[taskListId]/_detail/_components/_internal/HeaderSection/HeaderSection.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from "@storybook/nextjs";
 import HeaderSection from "./HeaderSection";
-import { TASK_DETAIL_MOCK_DATA } from "@/MOCK_DATA";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GetTaskDetailResponse } from "@/api/axios/task/_types";
 
 const queryClient = new QueryClient();
 
@@ -30,6 +30,51 @@ const MOCK_PATH = {
   id: 27453,
   teamId: 3449,
   taskListId: 4871,
+};
+
+const TASK_DETAIL_MOCK_DATA: GetTaskDetailResponse = {
+  id: 0,
+  name: "회의록 정리하기",
+  description: "팀 회의에서 나온 내용을 정리하는 작업입니다.",
+  displayIndex: 0,
+  commentCount: 2,
+
+  frequency: "DAILY",
+  recurringId: 0,
+
+  date: "2025-11-21T20:27:10.529Z",
+  doneAt: "2025-11-21T20:27:10.529Z",
+  updatedAt: "2025-11-21T20:27:10.529Z",
+  deletedAt: null,
+
+  doneBy: {
+    user: {
+      id: 1,
+      nickname: "짱구",
+      image: "/TEST_IMG/image-1.jpg",
+    },
+  },
+
+  writer: {
+    id: 2,
+    nickname: "철수",
+    image: "/TEST_IMG/image-1.jpg",
+  },
+
+  recurring: {
+    id: 0,
+    name: "회의록 정리하기",
+    description: "팀 회의에서 나온 내용을 정리하는 작업입니다.",
+    createdAt: "2025-11-21T20:27:10.529Z",
+    updatedAt: "2025-11-21T20:27:10.529Z",
+    startDate: "2025-11-21T20:27:10.529Z",
+    frequencyType: "DAILY",
+    weekDays: [],
+    monthDay: null,
+    taskListId: 0,
+    groupId: 0,
+    writerId: 0,
+  },
 };
 
 export const Default: Story = {
